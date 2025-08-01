af2c091e7760b833af6a42d59fd329d8ec8a1f72
Gayathri <165009830+Gayathri-D3@users.noreply.github.com>
2025-08-01 12:29:21 +0530

    Update App.js

--- 22BQ1A54D3/Frontend Test Submission/src/App.js
+++ 22BQ1A54D3/Frontend Test Submission/src/App.js
@@ -5,7 +5,7 @@ import { Log } from '../../LoggingMiddleware/loggingMiddleware';
 function App() {
   const [url, setUrl] = useState('');
   const handleSubmit = async () => {
-    await Log("frontend", "info", "UI", `User submitted URL: ${url}`, "YOUR_TOKEN_HERE");
+    await Log("frontend", "info", "UI", `User submitted URL: ${url}`, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJyZWRkZW1nYXlhdGhyaXJlZGR5MkBnbWFpbC5jb20iLCJleHAiOjE3NTQwMjkwMTEsImlhdCI6MTc1NDAyODExMSwiaXNzIjoiQWZmb3JkIE1lZGljYWwgVGVjaG5vbG9naWVzIFByaXZhdGUgTGltaXRlZCIsImp0aSI6ImVhNjVlMjI5LTg4MzEtNDA0Yy05OTdjLTIzOTdjOTllYzY3MSIsImxvY2FsZSI6ImVuLUlOIiwibmFtZSI6InJlZGRlbSBnYXlhdGhyaSByZWRkeSIsInN1YiI6IjhjZWI2MTllLTdjYTktNGIxMy05YjU0LTIxMjdkOWIwZTMwNCJ9LCJlbWFpbCI6InJlZGRlbWdheWF0aHJpcmVkZHkyQGdtYWlsLmNvbSIsIm5hbWUiOiJyZWRkZW0gZ2F5YXRocmkgcmVkZHkiLCJyb2xsTm8iOiIyMmJxMWE1NGQzIiwiYWNjZXNzQ29kZSI6IlBuVkJGViIsImNsaWVudElEIjoiOGNlYjYxOWUtN2NhOS00YjEzLTliNTQtMjEyN2Q5YjBlMzA0IiwiY2xpZW50U2VjcmV0IjoiZGt3UXhBdUpDZndnY211ZCJ9.ELRWSzDdGOCEVXOqG_UgblhaALyMuHMQIxUxwZPSYro");
     alert('URL submitted');
   };
 
